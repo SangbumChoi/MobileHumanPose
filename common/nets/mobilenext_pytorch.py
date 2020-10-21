6e1ab1453514a21ffe8122de7e23f2f37e43edf4
SangbumChoi <danielsejong55@gmail.com>
2020-10-21 15:38:31 +0900

    fix : add init_weights

--- common/nets/mobilenext_pytorch.py
+++ common/nets/mobilenext_pytorch.py
@@ -104,6 +104,20 @@ class MobileNeXt(nn.Module):
 
         self.sandglass_conv = nn.Sequential(*layers)
 
+    # weight initialization
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+
     def forward(self, x):
         x = self.block1(x)
         x = self.sandglass_conv(x)
