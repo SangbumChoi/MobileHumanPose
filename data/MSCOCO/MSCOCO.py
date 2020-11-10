38232a5600c919b71b31dcfe3c0e00d26f59eb7e
SangbumChoi <danielsejong55@gmail.com>
2020-11-10 14:54:43 +0900

    fix : root repostory

--- data/MSCOCO/MSCOCO.py
+++ data/MSCOCO/MSCOCO.py
@@ -15,10 +15,10 @@ from utils.vis import vis_keypoints, vis_3d_skeleton
 class MSCOCO:
     def __init__(self, data_split):
         self.data_split = data_split
-        self.img_dir = osp.join('..', 'data', 'MSCOCO', 'images')
-        self.train_annot_path = osp.join('..', 'data', 'MSCOCO', 'annotations', 'person_keypoints_train2017.json')
-        self.test_annot_path = osp.join('..', 'data', 'MSCOCO', 'annotations', 'person_keypoints_val2017.json')
-        self.human_3d_bbox_root_dir = osp.join('..', 'data', 'MSCOCO', 'bbox_root', 'bbox_root_coco_output.json')
+        self.img_dir = osp.join('/', 'data', 'MSCOCO', 'images')
+        self.train_annot_path = osp.join('/', 'data', 'MSCOCO', 'annotations', 'person_keypoints_train2017.json')
+        self.test_annot_path = osp.join('/', 'data', 'MSCOCO', 'annotations', 'person_keypoints_val2017.json')
+        self.human_3d_bbox_root_dir = osp.join('/', 'data', 'MSCOCO', 'bbox_root', 'bbox_root_coco_output.json')
         
         if self.data_split == 'train':
             self.joint_num = 19 # original: 17, but manually added 'Thorax', 'Pelvis'
